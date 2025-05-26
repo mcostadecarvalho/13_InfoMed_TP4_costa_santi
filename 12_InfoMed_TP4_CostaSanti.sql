2b8696c50b555ca09e9f35ab83a49faa0737c0f9
mcostadecarvalho <mcostadecarvalho@itba.edu.ar>
2025-05-25 23:12:46 -0300

    Add files via upload

---
+++ 12_InfoMed_TP4_CostaSanti.sql
@@ -0,0 +1,7 @@
+SELECT m.nombre AS nombre_medicamento, COUNT(*) AS veces_prescrito
+FROM recetas r
+JOIN medicamentos m ON r.id_medicamento = m.id_medicamento
+WHERE r.id_medico = 2
+GROUP BY m.nombre
+HAVING COUNT(*) > 1
+ORDER BY veces_prescrito DESC;
